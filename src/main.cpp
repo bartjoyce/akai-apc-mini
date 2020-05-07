139b3726c3ab8494a96f6ea1507c5c13e8a239c4
Bartholomew Joyce <bartholomew.michael.joyce@gmail.com>
2020-05-07 15:15:58 +0200

    Switched from polling to callback

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,51 @@
-#include <iostream>
-#include <cstdlib>
+#include <stdio.h>
+#include <unistd.h>
 #include <RtMidi.h>
-#include <chrono>
-
-struct Message {
-    Message(unsigned char b0, unsigned char b1, unsigned char b2, int delta_time)
-        : bytes { b0, b1, b2 }, delta_time(delta_time) {}
-    std::vector<unsigned char> bytes;
-    int delta_time;
-};
-
-void sleep_for(float time) {
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    while (true) {
-        auto now = std::chrono::high_resolution_clock::now();
-
-        auto time_diff = std::chrono::duration_cast<std::chrono::microseconds>(now - start_time).count();
-        if (time_diff >= time) {
-            break;
-        }
-
-    }
-}
 
 bool done;
 static void interrupt(int ignore){
     done = true;
 }
 
-void enable_if_true(RtMidiOut* midi_out, int* states, int button, bool condition) {
-    int state = condition ? 0x01 : 0x00;
-    if (states[button] != state) {
-        states[button]  = state;
-        std::vector<unsigned char> message = { 0x90, (unsigned char)button, (unsigned char)state };
+static RtMidiIn*  midi_in;
+static RtMidiOut* midi_out;
+static int states[256];
+
+void midi_in_callback(double time_stamp, std::vector<unsigned char>* message_ptr, void* user_data) {
+    auto& message = *message_ptr;
+    if (message.empty() || message.size() < 3) {
+        return;
+    }
+
+    // Grid buttons:
+    //  Note on 0x00 : no lights
+    //  Note on 0x01 : green  solid
+    //  Note on 0x02 : green  flashing
+    //  Note on 0x03 : red    solid
+    //  Note on 0x04 : red    flashing
+    //  Note on 0x05 : yellow solid
+    //  Note on 0x06 : yellow flashing
+
+    if (message[0] == 0x90) {
+        // Note on
+        printf("NOTE ON  %02x %02x\n", (int)message[1], (int)message[2]);
+        message[2] = states[message[1]] = (states[message[1]] + 1) % 0x07;
         midi_out->sendMessage(&message);
+    } else if (message[0] == 0x80) {
+        // Note off
+        printf("NOTE OFF %02x %02x\n", (int)message[1], (int)message[2]);
+    } else if (message[0] == 0xb0) {
+        // Control message
+        printf("CONTROL  %02x %02x\n", (int)message[1], (int)message[2]);
     }
 }
 
 int main(int argc, const char** argv) {
-    RtMidiIn*  midi_in  = new RtMidiIn();
-    RtMidiOut* midi_out = new RtMidiOut();
     std::vector<unsigned char> message;
-    int states[256];
+
+    // Init global variables
+    midi_in  = new RtMidiIn;
+    midi_out = new RtMidiOut;
     for (int i = 0x00; i < 0xff; ++i) {
         states[i] = 0;
     }
@@ -74,42 +77,11 @@ int main(int argc, const char** argv) {
         goto cleanup;
     }
 
+    midi_in->setCallback(&midi_in_callback, NULL);
+
+    // Loop until interrupted
     while (!done) {
-        midi_in->getMessage(&message);
-        if (message.empty() || message.size() < 3) {
-            continue;
-        }
-
-        // Note on 0x01 : solid green
-        // Note on 0x02 : flashing green
-
-        // Note on 0x03 : solid red
-        // Note on 0x04 : flashing red
-
-        // Note on 0x05 : solid yellow
-        // Note on 0x06 : flashing yellow
-
-        if (message[0] == 0x90) {
-            // Note on
-            printf("NOTE ON  %02x %02x\n", (int)message[1], (int)message[2]);
-            message[2] = states[message[1]] = (states[message[1]] + 1) % 0x07;
-            midi_out->sendMessage(&message);
-        } else if (message[0] == 0x80) {
-            // Note off
-            printf("NOTE OFF %02x %02x\n", (int)message[1], (int)message[2]);
-        } else if (message[0] == 0xb0) {
-            // Control message
-            printf("CONTROL  %02x %02x\n", (int)message[1], (int)message[2]);
-            enable_if_true(midi_out, states, 0x00, 0x10 <= message[2]);
-            enable_if_true(midi_out, states, 0x08, 0x20 <= message[2]);
-            enable_if_true(midi_out, states, 0x10, 0x30 <= message[2]);
-            enable_if_true(midi_out, states, 0x18, 0x40 <= message[2]);
-            enable_if_true(midi_out, states, 0x20, 0x50 <= message[2]);
-            enable_if_true(midi_out, states, 0x28, 0x60 <= message[2]);
-            enable_if_true(midi_out, states, 0x30, 0x70 <= message[2]);
-            enable_if_true(midi_out, states, 0x38, 0x7f <= message[2]);
-        }
-
+        sleep(1);
     }
 
 cleanup:
